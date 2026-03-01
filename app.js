5897b9f2b3a7959752c07e573540e293b8038d4e
daniel-lee3 <daniel-lee3@live.nmit.ac.nz>
2026-03-02 11:29:06 +1300

    app.js now produces an output

--- app.js
+++ app.js
@@ -8,4 +8,6 @@ score = 10;             // OK - reassignment allowed for let
 const MAX = 100;
 // MAX = 200;           // TypeError: Assignment to constant variable
 
-let homeAddress = "Your home address"; // I declared the variable homeAddress and assigned a string value to it
+let homeAddress = "In your walls"; // I declared the variable homeAddress and assigned a string value to it
+
+console.log(`My name is ${name} and I live on ${homeAddress}`)
